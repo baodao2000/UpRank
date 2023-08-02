a12f6da5147cfd74706297733d2bf47d96bf6849
Mac <you@example.com>
2023-08-02 13:59:45 +0700

    hh

--- src/components/Currency.tsx
+++ src/components/Currency.tsx
@@ -42,7 +42,7 @@ const Title = styled(Text)`
   line-height: 28px;
 `
 const Label = styled(Text)`
-  color: #21cf9a;
+  color: #3aab9d;
   /* Text md/regular */
   font-family: Inter;
   font-size: 16px;
@@ -52,82 +52,34 @@ const Label = styled(Text)`
 `
 
 export const CurrencyExchange = () => {
-  const poolsV4Contract = usePoolsV4Contract()
-  const [matic, setMatic] = useState(0)
-  const [trend, setTrend] = useState(0)
-  const [maticPrice, setMaticPrice] = useState(0)
-  const [manticPriceChange, setMaticPriceChange] = useState(0)
-  const [trendPrice, setTrendPrice] = useState(0)
-  const [trendPriceChange, setTrendPriceChange] = useState(0)
+  const [price, setPrice] = useState({
+    matic: 0,
+    maticChange: 0,
+    trend: 0,
+    trendChange: 0,
+  })
 
-  const baseURL = 'https://services.intotheblock.com/api/MATIC/overview'
   useEffect(() => {
-    // getCurrencyExchange()
-    axios.get(baseURL).then((data) => {
-      setMaticPrice(data.data.price)
-      setMaticPriceChange(data.data.priceChange)
-    })
     getPriceTrend()
   }, [])
   const getPriceTrend = async () => {
-    var myHeaders = new Headers()
-    myHeaders.append('accept', 'application/json')
-    myHeaders.append('X-API-Key', '01e54e9712d16936f7a4a333fc6c789f')
-    myHeaders.append(
-      'Cookie',
-      '__cf_bm=lf5QBEfZEKBjXu5ky72.0MldofFFndhty11kedx5PiY-1690950108-0-AaK5GEstLN7S5SFxUQ43cIX6AwZ3LpTs2Be1qmfL9msXWHBT7JDFBmn1LJelV7lkYlFVPOjAvj/FWT3SwZFBcZM=',
-    )
-
-    axios({
-      method: 'get',
-      url: 'https://api.dextools.io/v1/pair?chain=polygon&address=0x6e430d59ba145c59b73a6db674fe3d53c1f31cae',
-      headers: {
-        accept: 'application/json',
-        'X-API-Key': '01e54e9712d16936f7a4a333fc6c789f',
-        Cookie:
-          '__cf_bm=lf5QBEfZEKBjXu5ky72.0MldofFFndhty11kedx5PiY-1690950108-0-AaK5GEstLN7S5SFxUQ43cIX6AwZ3LpTs2Be1qmfL9msXWHBT7JDFBmn1LJelV7lkYlFVPOjAvj/FWT3SwZFBcZM=',
-        Referer: 'https://trendydefi.com/',
-      },
+    const baseURL = 'https://services.intotheblock.com/api/MATIC/overview'
+    const [trend, matic] = await Promise.all([
+      fetch('https://api.trendydefi.com/price/trend').then((rs) => rs.json()),
+      axios.get(baseURL),
+    ])
+    setPrice({
+      matic: matic.data.price,
+      maticChange: matic.data.priceChange,
+      trend: trend.data.price,
+      trendChange: trend.data.price24h.priceChain,
     })
-      .then(function (response) {
-        console.log(JSON.stringify(response.data))
-      })
-      .catch(function (error) {
-        console.log(error)
-      })
-    // await fetch('https://api.dextools.io/v1/pair?chain=polygon&address=0x6e430d59ba145c59b73a6db674fe3d53c1f31cae', {
-    //     headers: {
-    //         accept: 'application/json',
-    //       'X-API-Key': '01e54e9712d16936f7a4a333fc6c789f',
-    //         redirect: 'follow'
-    //     },
-    //   }).then((data) => {
-    //     // setMaticPrice(data.data.price)
-    //     // setMaticPriceChange(data.data.priceChange)
-    //     console.log(data)
-    //   })
   }
-  const getCurrencyExchange = async () => {
-    const trend2USDT = await poolsV4Contract.TREND2USDT()
-    // const matic2USDT = await poolsV4Contract.MATIC2USDT()
-    // setMatic(1 / Number(formatEther(matic2USDT)))
-    // setTrend(1 / Number(formatEther(trend2USDT)))
-  }
+
   return (
     <Wrapper>
       <Card>
-        <Title>
-          <CountUp
-            separator=","
-            start={0}
-            preserveValue
-            delay={0}
-            // end={Number(Number(balance) * rateBnbUsd)}
-            decimals={3}
-            duration={1}
-            end={Number(maticPrice)}
-          />
-        </Title>
+        <Title>1</Title>
         <div
           style={{
             background: 'black',
@@ -143,13 +95,26 @@ export const CurrencyExchange = () => {
           <img width="18px" height="16px" src="/images/V3/V3icon.png" />
         </div>
         <span style={{ color: 'rgba(173, 171, 178, 1)', fontSize: '16px' }}>=</span>
-        <Title>$1 </Title>
+        <Title>
+          $
+          <CountUp
+            separator=","
+            start={0}
+            preserveValue
+            delay={0}
+            // end={Number(Number(balance) * rateBnbUsd)}
+            decimals={3}
+            duration={1}
+            end={Number(price.matic)}
+          />{' '}
+        </Title>
 
-        {manticPriceChange > 0 ? (
+        {price.maticChange > 0 ? (
           <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
             <img width="16px" height="16px" src="/images/V3/Arrow.png" />
 
             <Label>
+              +
               <CountUp
                 separator=","
                 start={0}
@@ -158,7 +123,7 @@ export const CurrencyExchange = () => {
                 // end={Number(Number(balance) * rateBnbUsd)}
                 decimals={3}
                 duration={1}
-                end={Number(manticPriceChange)}
+                end={Number(price.maticChange)}
               />
               %
             </Label>
@@ -168,7 +133,7 @@ export const CurrencyExchange = () => {
             <img width="16px" height="16px" src="/images/V3/downArrow.png" />
 
             <Label style={{ color: '#E30721' }}>
-              {' '}
+              -
               <CountUp
                 separator=","
                 start={0}
@@ -177,7 +142,7 @@ export const CurrencyExchange = () => {
                 // end={Number(Number(balance) * rateBnbUsd)}
                 decimals={3}
                 duration={1}
-                end={Number(manticPriceChange)}
+                end={Number(price.maticChange)}
               />
               %
             </Label>
@@ -185,18 +150,7 @@ export const CurrencyExchange = () => {
         )}
       </Card>
       <Card>
-        <Title>
-          <CountUp
-            separator=","
-            start={0}
-            preserveValue
-            delay={0}
-            // end={Number(Number(balance) * rateBnbUsd)}
-            decimals={3}
-            duration={1}
-            end={Number(trend)}
-          />
-        </Title>
+        <Title>1</Title>
         <div
           style={{
             background: 'black',
@@ -212,11 +166,58 @@ export const CurrencyExchange = () => {
           <img width="18px" height="16px" src="/images/trendyloopcoin1.png" />
         </div>
         <span style={{ color: 'rgba(173, 171, 178, 1)', fontSize: '16px' }}>=</span>
-        <Title>$1 </Title>
-        <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
-          <img width="16px" height="16px" src="/images/V3/arrowUp.png" />
-          <Label>+0.030%</Label>
-        </div>
+        <Title>
+          $
+          <CountUp
+            separator=","
+            start={0}
+            preserveValue
+            delay={0}
+            // end={Number(Number(balance) * rateBnbUsd)}
+            decimals={3}
+            duration={1}
+            end={Number(price.trend)}
+          />{' '}
+        </Title>
+        {price.trendChange > 0 ? (
+          <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+            <img width="16px" height="16px" src="/images/V3/Arrow.png" />
+
+            <Label>
+              +
+              <CountUp
+                separator=","
+                start={0}
+                preserveValue
+                delay={0}
+                // end={Number(Number(balance) * rateBnbUsd)}
+                decimals={3}
+                duration={1}
+                end={Number(price.trendChange)}
+              />
+              %
+            </Label>
+          </div>
+        ) : (
+          <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+            <img width="16px" height="16px" src="/images/V3/downArrow.png" />
+
+            <Label style={{ color: '#E30721' }}>
+              -
+              <CountUp
+                separator=","
+                start={0}
+                preserveValue
+                delay={0}
+                // end={Number(Number(balance) * rateBnbUsd)}
+                decimals={3}
+                duration={1}
+                end={Number(price.trendChange)}
+              />
+              %
+            </Label>
+          </div>
+        )}
       </Card>
     </Wrapper>
   )
